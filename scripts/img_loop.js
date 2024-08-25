ae91fe95d6b5503b2efaceb99d201fe8ed5fb191
Ciaran Saundry <saundryc@stu.otc.school.nz>
2024-08-26 09:59:45 +1200

    Final

--- scripts/img_loop.js
+++ scripts/img_loop.js
@@ -1,14 +1,14 @@
 function start() {
-    var rotator = document.getElementById('maerotator');  
-    var imageDir = 'assets/img/cycles/';                          
-    var delayInSeconds = 1;                            
+    var rotator = document.getElementById('maerotator');
+    var imageDir = 'assets/img/cycles/';
+    var delayInSeconds = 1.5;
 
     var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
 
     var num = 0;
     var changeImage = function() {
         var len = images.length;
-        rotator.src = imageDir + images[num++];
+        rotator.style.backgroundImage = "url(" + imageDir + images[num++] + ")";
         if (num == len) {
             num = 0;
         }
